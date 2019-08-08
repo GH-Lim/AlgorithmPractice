8a06f2f4f247310b3f666a9da86ecb0d822f87d7
임건혁 <gunhyuck11@naver.com>
2019-08-08 23:48:10 +0900

    Solve algo problem
     - 6190

--- problems/D3/6190_정곤이의단조증가하는수.py
+++ problems/D3/6190_정곤이의단조증가하는수.py
@@ -1,16 +1,24 @@
 T = int(input())
 
+
+def isMono(number):
+    for i in range(len(number)-1):
+        if int(number[i]) > int(number[i+1]):
+            return False
+    return True
+
+
 for tc in range(1, T+1):
     N = int(input())
     max_num = 0
-    for n in range(N):
-        nums = input().split()
-        for num in nums:
-            flag = True
-            for i in range(len(num)):
-                if int(num[i]) > int(num[i+1]):
-                    flag = False
-                    break
-            if flag:
-                max_num = max_num if max_num > num else num
+    A = list(map(int, input().split()))
+    noMono = True
+    for i in range(N-1):
+        for j in range(i+1, N):
+            target = str(A[i] * A[j])
+            if isMono(target):
+                noMono = False
+                max_num = max_num if max_num > int(target) else int(target)
+    result = -1 if noMono else max_num
 
+    print('#{} {}'.format(tc, result))
