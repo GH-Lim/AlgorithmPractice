1341dbc6456f8c0d25091b0b81c310f700ddf2ae
임건혁 <gunhyuck11@naver.com>
2019-08-08 19:47:44 +0900

    Solve 7675

--- problems/D3/7675_통역사성경이.py
+++ problems/D3/7675_통역사성경이.py
@@ -1,7 +1,7 @@
 import sys
 sys.stdin = open('input_7675.txt', 'r')
 
-print('A'.isupper())
+print('Aa'.isalpha())
 T = int(input())
 
 
@@ -9,13 +9,16 @@ def isName(word):
     if len(word) == 1:
         return word.isupper()
     else:
-        if word[0].islower():
-            return False
-        else:
-            for i in range(1, len(word)):
-                if word[i].isupper() or word[i].isdigit():
+        if word.isalpha():
+            if word[0].islower():
+                return False
+            else:
+                if word[1:].islower():
+                    return True
+                else:
                     return False
-        return True
+        else:
+            return False
 
 
 pp = ['.', '?', '!']
@@ -27,9 +30,9 @@ for tc in range(1, T + 1):
         # n = sentence.count(pp[i])
         for _ in range(sentences.count(pp[i])):
             sentences = sentences.replace(pp[i], ':')
-    sentences = sentences.strip(':').split(':')
+    sentences = sentences.split(':')
     for i in range(N):
-        words = sentences[i].strip().split()
+        words = sentences[i].strip(':').strip().split()
         for word in words:
             if isName(word):
                 name_count[i] += 1
