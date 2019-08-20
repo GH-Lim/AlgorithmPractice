306b10b18fbde11912c763de7ffb9dedd263b0c7
임건혁 <gunhyuck11@naver.com>
2019-08-20 10:08:17 +0900

    Upload

--- problems/examples/연습0819.py
+++ problems/examples/연습0819.py
@@ -10,7 +10,7 @@ def dfs(v):
         if not visited[v-1]:
             visited[v-1] = True
             for i in range(7, 0, -1):
-                if arr[v-1][i-1] and not visited[i-1]:
+                if arr[v][i] and not visited[i]:
                     top += 1
                     stack[top] = i
             result.append(v)
@@ -19,13 +19,13 @@ def dfs(v):
 
 a = [1, 2, 1, 3, 2, 4, 2, 5, 4, 6, 5, 6, 6, 7, 3, 7]
 # node = [1, 2, 3, 4, 5, 6, 7]
-arr = [[0 for _ in range(7)] for _ in range(7)]
+arr = [[0 for _ in range(8)] for _ in range(8)]
 for i in range(len(a) // 2):
-    arr[a[2 * i]-1][a[2 * i + 1]-1] = 1
-    arr[a[2 * i + 1]-1][a[2 * i]-1] = 1
+    arr[a[2 * i]][a[2 * i + 1]] = 1
+    arr[a[2 * i + 1]][a[2 * i]] = 1
 stack = [0] * 7
 top = -1
-visited = [False] * 7
+visited = [False] * 8
 # visited[0] = True
 
 dfs(1)
--- problems/examples/연습0819_1.py
+++ problems/examples/연습0819_1.py
@@ -1,11 +1,17 @@
-def dfs_recursive(G, v):
-    visited[v-1] = True
+def dfs_recursive(v):
+    print(v)
+    visited[v] = True
+
+    for i in range(1, 8):
+        if G[v][i] and not visited[i]:
+            dfs_recursive(i)
 
 
 a = [1, 2, 1, 3, 2, 4, 2, 5, 4, 6, 5, 6, 6, 7, 3, 7]
-arr = [[0 for _ in range(7)] for _ in range(7)]
+G = [[0 for _ in range(8)] for _ in range(8)]
 for i in range(len(a) // 2):
-    arr[a[2 * i]-1][a[2 * i + 1]-1] = 1
-    arr[a[2 * i + 1]-1][a[2 * i]-1] = 1
+    G[a[2 * i]][a[2 * i + 1]] = 1
+    G[a[2 * i + 1]][a[2 * i]] = 1
 
-visited = [False] * 7
+visited = [False] * 8
+dfs_recursive(1)
