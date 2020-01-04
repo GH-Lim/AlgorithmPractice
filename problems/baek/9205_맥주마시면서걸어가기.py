d1e0b3ad9be05b5f598c674559c080b22d7b374b
gh-lim <gunhyuck11@gmail.com>
2020-01-04 23:31:46 +0900

    0104

--- problems/baek/9205_맥주마시면서걸어가기.py
+++ problems/baek/9205_맥주마시면서걸어가기.py
@@ -1,7 +1,8 @@
-# inf = 2e10
-# for tc in range(1, int(input()) + 1):
-#     n = int(input())
-#     points = [list(map(int, input().split())) for _ in range(n + 2)]
-#     G = [[inf] * (n + 2) for _ in range(n + 2)]
-#     for s in range(n + 2):
-#         for e in range(n + 2):
+inf = 2e10
+for tc in range(1, int(input()) + 1):
+    n = int(input())
+    points = [list(map(int, input().split())) for _ in range(n + 2)]
+    G = [[inf] * (n + 2) for _ in range(n + 2)]
+    for s in range(n + 2):
+        for e in range(n + 2):
+            
