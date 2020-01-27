9afa9b5b9d5770534f6d79ee14d00da2bed7ed20
gh-lim <gunhyuck11@gmail.com>
2020-01-27 23:28:27 +0900

    0127

--- problems/baek/18224_미로에갇힌건우.py
+++ problems/baek/18224_미로에갇힌건우.py
@@ -27,25 +27,23 @@ while q:
         if time:
             day += 1
         time = not time
-    sub_visited = {}
     for _ in range(len(q)):
         y, x = q.popleft()
         for d in range(4):
             ny, nx = y + dy[d], x + dx[d]
             if (ny, nx) == (n - 1, n - 1): break
-            if 0 <= ny < n and 0 <= nx < n and (ny, nx) not in sub_visited:
+            if 0 <= ny < n and 0 <= nx < n and (ny, nx, cnt, time) not in visited:
                 if not maze[ny][nx]:
                     q.append((ny, nx))
-                    sub_visited[(ny, nx)] = 1
-                    visited[(ny, nx)] = 1
+                    visited[(ny, nx, cnt, time)] = 1
                 else:
                     if time:
                         temp = jump_wall(ny, nx, d)
-                        if temp and temp not in visited:
-                            if temp == (n - 1, n - 1): break
-                            q.append(temp)
-                            visited[temp] = 1
-                            sub_visited[temp] = 1
+                        if temp:
+                            if (temp[0], temp[1], cnt, time) not in visited:
+                                if temp == (n - 1, n - 1): break
+                                q.append(temp)
+                                visited[(temp[0], temp[1], cnt, time)] = 1
         else: continue # 출구 못찾았으면
         break
     else:
