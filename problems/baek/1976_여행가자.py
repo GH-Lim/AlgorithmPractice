da953dab25f08e8725570e72b4706ed883c65a1f
임건혁 <gunhyuck11@naver.com>
2020-02-03 08:34:49 +0900

    merge

---
+++ problems/baek/1976_여행가자.py
@@ -0,0 +1,9 @@
+N = int(input())
+M = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+plan = list(map(int, input().split()))
+
+def
+for i in range(N):
+    for j in range(N):
