f2fcbed33939d24c9c967b3cf135da16b4ffacb8
gh-lim <gunhyuck11@gmail.com>
2020-02-09 23:06:16 +0900

    0209

--- problems/baek/17137_사탕놀이.py
+++ problems/baek/17137_사탕놀이.py
@@ -1,10 +1,10 @@
-def comb(k, pre, arr):
+def comb(k, pre):
     global cnt
     if k == n:
         cnt += 1
         return
     for i in range(pre, x[k] + 1):
-        comb(k + 1, i, arr + i*(10**k))
+        comb(k + 1, i)
 
 
 for tc in range(int(input())):
@@ -13,5 +13,5 @@ for tc in range(int(input())):
     x = list(map(int, input().split()))
     x.sort()
     cnt = 0
-    comb(0, 1, 0)
+    comb(0, 1)
     print(n*cnt % 1000000007)
