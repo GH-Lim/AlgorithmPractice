b589d914cc7141c31ebb913d73ae9ea54308a3c8
GH-Lim <52685236+GH-Lim@users.noreply.github.com>
2020-02-29 22:04:49 +0900

    Update 5658_보물상자비밀번호.py

--- problems/AD/5658_보물상자비밀번호.py
+++ problems/AD/5658_보물상자비밀번호.py
@@ -8,14 +8,14 @@ for tc in range(1, T + 1):
     N, K = map(int, input().split())
     nums = deque(input())
 
-    size = N // 4
+    size = N // 4  # 4 모서리
     results = []
     for r in range(size):
-        nums.rotate(1)
+        nums.rotate(1)  # 1칸씩 회전
         for i in range(4):
-            num = ''
+            num = ''  # 빈 string
             for j in range(i * size, (i + 1) * size):
-                num += nums[j]
+                num += nums[j]  # 하나씩 더한다.
             if num not in results:
                 results.append(num)
     results.sort(reverse=True)
