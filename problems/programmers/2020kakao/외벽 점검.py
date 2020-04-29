179597abba5f22e4782f7d054d944a851e0b4b1c
GH-Lim <gunhyuck11@gmail.com>
2020-04-29 23:56:47 +0900

    외벽 점검

--- problems/programmers/2020kakao/외벽 점검.py
+++ problems/programmers/2020kakao/외벽 점검.py
@@ -2,5 +2,10 @@ from collections import deque
 from itertools import permutations
 def solution(n, weak, dist):
     answer = 0
+    for p_dist in permutations(dist, len(dist)):
+        cnt = 0
+        while p_dist[cnt]
+
+
 
     return answer
