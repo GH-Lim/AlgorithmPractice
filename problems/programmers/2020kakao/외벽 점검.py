a4f388588aaf4d821849357a597c2d64cc414dac
GH-Lim <gunhyuck11@gmail.com>
2020-05-03 22:02:35 +0900

    외벽점검 restart

--- problems/programmers/2020kakao/외벽 점검.py
+++ problems/programmers/2020kakao/외벽 점검.py
@@ -2,11 +2,4 @@ from collections import deque
 from itertools import permutations
 def solution(n, weak, dist):
     answer = 0
-    for p_dist in permutations(dist, len(dist)):
-        cnt = 0
-        i = 0
-        
-
-
-
     return answer
