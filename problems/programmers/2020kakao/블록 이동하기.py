bafe9609791612d8b023da4903506b346c381dd9
GH-Lim <gunhyuck11@gmail.com>
2020-05-05 23:50:17 +0900

    블록 이동하기

--- problems/programmers/2020kakao/블록 이동하기.py
+++ problems/programmers/2020kakao/블록 이동하기.py
@@ -3,4 +3,8 @@ def solution(board):
     return answer
 
 
-def move(y1, x1, y2, x2):
+def move(y1, x1, y2, x2, visited):
+    for dy, dx in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+        ny1, nx1, ny2, nx2 = y1 + dy, x1 + dx, y2 + dy, x2 + dx
+        if (ny1, nx1, ny2, nx2) not in visited:
+            if
