ec75460f44820280a830fafeb9ad976f395a7bcd
GH-Lim <gunhyuck11@gmail.com>
2020-05-06 23:40:49 +0900

    외벽 점검

--- problems/programmers/2020kakao/블록 이동하기.py
+++ problems/programmers/2020kakao/블록 이동하기.py
@@ -7,4 +7,4 @@ def move(y1, x1, y2, x2, visited):
     for dy, dx in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
         ny1, nx1, ny2, nx2 = y1 + dy, x1 + dx, y2 + dy, x2 + dx
         if (ny1, nx1, ny2, nx2) not in visited:
-            if
+            if 
--- problems/programmers/2020kakao/외벽 점검.py
+++ problems/programmers/2020kakao/외벽 점검.py
@@ -1,5 +1,18 @@
 from collections import deque
 from itertools import permutations
 def solution(n, weak, dist):
-    answer = 0
+    answer = len(dist) + 1
+    weak = deque(weak)
+    dist_perms = permutations(dist)
+    for _ in range(len(dist)):
+        for d_perm in dist_perms:
+            i = 0
+            for d in d_perm:
+                while
+
+
+        weak.rotate(-1)
+        weak[-1] += n
     return answer
+
+solution(4, [1, 2], [1, 2])
