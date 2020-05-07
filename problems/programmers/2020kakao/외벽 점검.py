365ef94d619dd3ab972a1fd588378e88dba29fb2
GH-Lim <gunhyuck11@gmail.com>
2020-05-07 11:26:47 +0900

    외벽 점검

--- problems/programmers/2020kakao/외벽 점검.py
+++ problems/programmers/2020kakao/외벽 점검.py
@@ -3,16 +3,20 @@ from itertools import permutations
 def solution(n, weak, dist):
     answer = len(dist) + 1
     weak = deque(weak)
-    dist_perms = permutations(dist)
-    for _ in range(len(dist)):
+    dist_perms = list(permutations(dist))
+    for _ in range(len(weak)):
         for d_perm in dist_perms:
-            i = 0
-            for d in d_perm:
-                while
-
-
+            start_i = 0
+            for i in range(len(d_perm)):
+                for j in range(start_i + 1, len(weak)):
+                    if weak[start_i] + d_perm[i] >= weak[-1]:
+                        answer = min(answer, i + 1)
+                        break
+                    if weak[start_i] + d_perm[i] < weak[j]:
+                        start_i = j
+                        break
         weak.rotate(-1)
         weak[-1] += n
-    return answer
+    return answer if answer != len(dist) + 1 else -1
 
-solution(4, [1, 2], [1, 2])
+print(solution(12, [1, 3, 4, 9, 10], [3, 5, 7]))
