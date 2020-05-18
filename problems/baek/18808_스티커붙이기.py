308280dc13449e472884561664008b6d6ed27c54
GH-Lim <gunhyuck11@gmail.com>
2020-05-18 20:56:32 +0900

    0518

--- problems/baek/18808_스티커붙이기.py
+++ problems/baek/18808_스티커붙이기.py
@@ -6,9 +6,15 @@ stickers = []
 for _ in range(K):
     R, C = list(map(int, input().split()))
     sticker = [list(map(int, input().split())) for _ in range(R)]
-    stickers.append(sticker)
+    temp_sticker = {}
+    for i in range(R):
+        for j in range(C):
+            if sticker[i][j]:
+                temp_sticker[(i, j)] = 1
+    stickers.append(temp_sticker)
 
-def spin(stick):
-    for i in range(len(stick)):
-        for j in range(len(stick[0])):
-            pass
+
+def spin(sticker):
+    temp_sticker = {}
+    for i, j in sticker:
+        temp_sticker[(j, i - 1 - j)]
