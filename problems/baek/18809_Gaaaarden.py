a265be0595c39317006bfea0860c76c61e769b79
GH-Lim <gunhyuck11@gmail.com>
2020-05-25 23:26:25 +0900

    0525

--- problems/baek/18809_Gaaaarden.py
+++ problems/baek/18809_Gaaaarden.py
@@ -1,4 +1,16 @@
+from itertools import combinations
 N, M, G, R = map(int, input().split())
 
 garden = [list(map(int, input().split())) for _ in range(N)]
 
+yellow = []
+for i in range(N):
+    for j in range(M):
+        if garden[i][j] == 2:
+            yellow.append((i, j))
+
+for candidate in combinations(yellow, G + R):
+    pass
+
+def inject(candi, G, R):
+    pass
