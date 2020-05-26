766c0aa5e3d30b00f80a2fd9266da2829fd68f46
GH-Lim <gunhyuck11@gmail.com>
2020-05-26 23:49:01 +0900

    0525

--- problems/baek/18809_Gaaaarden.py
+++ problems/baek/18809_Gaaaarden.py
@@ -12,5 +12,36 @@ for i in range(N):
 for candidate in combinations(yellow, G + R):
     pass
 
-def inject(candi, G, R):
-    pass
+
+def inject(candi, G, R, com):
+    if G == R == 0:
+        start(candi, com)
+        return
+    if G:
+        inject(candi, G - 1, R, com + [0])
+
+    if R:
+        inject(candi, G, R - 1, com + [1])
+
+def start(candi, com):
+    green = {}
+    red = {}
+    flower = {}
+    for ca, ci in zip(candi, com):
+        if ci == 0:
+            green[ca] = 1
+        else:
+            red[ca] = 1
+
+    while True:
+        temp = {}
+        for g in green:
+            pass
+
+        for r in red:
+            y, x = r
+            for dy, dx in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                ny, nx = y + dy, x + dx
+            if r in green:
+                flower[r] = 1
+            pass
