8b3c38827ed462ba636bac514c83fd413e4f2f8c
GH-Lim <gunhyuck11@gmail.com>
2020-06-21 23:44:03 +0900

    후보키

--- problems/programmers/2019kakao/후보키.py
+++ problems/programmers/2019kakao/후보키.py
@@ -1,3 +1,18 @@
+from itertools import combinations
 def solution(relation):
-    answer = 0
-    return answer
+    col_len = len(relation[0])
+    attribute_index = range(col_len)
+    check_list = []
+    #유일성 검사
+    for num in range(1,col_len+1):
+        comb_attribute = combinations(attribute_index,num)
+        for check_comb in list(comb_attribute):
+            all_attr_list = [tuple(item[index] for index in check_comb) for item in relation]
+            if len(set(all_attr_list)) != len(relation): continue
+            else: check_list.append(set(check_comb))
+    #최소성 검사
+    for item1 in check_list[:]:
+        for item2 in check_list[:]:
+            if item1 != item2:
+                if item1 == item1 & item2: check_list.remove(item2)
+    return len(check_list)
