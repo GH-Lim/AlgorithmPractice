b800a8493c8c7cadf9e6cd6885656dd36d1fbdfc
GH-Lim <gunhyuck11@gmail.com>
2020-06-22 23:52:23 +0900

    n진수 게임 다른 풀이

--- problems/programmers/2018kakao/n진수 게임.py
+++ problems/programmers/2018kakao/n진수 게임.py
@@ -20,3 +20,24 @@ def trans_n(n, num):
         num //= n
     encoded += N[num % n]
     return encoded[::-1]
+
+
+# def solution(n, t, m, p):
+#     m_chars = '0123456789ABCDEF'
+#
+#     def _rep(numb, to):
+#         j, k = divmod(numb, to)
+#         i = m_chars[k]
+#         return ('' if j == 0 else _rep(j, to)) + i
+#
+#     numbers = ''
+#     for i in range(0, t * m):
+#         numbers += _rep(i, n)
+#         if len(numbers) >= t * m:
+#             break
+#
+#     result = ''
+#     for i in range(p - 1, t * m, m):
+#         result += numbers[i]
+#
+#     return result
